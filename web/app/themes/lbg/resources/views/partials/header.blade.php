d6c9ffeedef9ae20ca8269f2f9ee2db0b290d5b0
aitor <aitor@e451.net>
2020-06-08 17:26:01 +0200

    Amburger

--- web/app/themes/lbg/resources/views/partials/header.blade.php
+++ web/app/themes/lbg/resources/views/partials/header.blade.php
@@ -7,4 +7,9 @@
       @endif
     </nav>
   </div>
+  <button class="hamburger hamburger--spin" type="button">
+    <span class="hamburger-box">
+      <span class="hamburger-inner"></span>
+    </span>
+  </button>  
 </header>
