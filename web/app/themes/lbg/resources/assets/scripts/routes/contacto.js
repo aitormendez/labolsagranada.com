5e928baff831222c13bc9f40c06da5c636d11162
aitor <aitor@e451.net>
2020-06-25 11:37:18 +0200

    Leaflet map 2

--- web/app/themes/lbg/resources/assets/scripts/routes/contacto.js
+++ web/app/themes/lbg/resources/assets/scripts/routes/contacto.js
@@ -1,10 +1,26 @@
-import L from 'leaflet';
+import L from 'leaflet/dist/leaflet.js';
+// import 'leaflet.fullscreen/Control.FullScreen';
+
+/* eslint-disable no-unused-vars */
 
 export default {
   init() {
     // JavaScript to be fired on the about us page
 
-    L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map', {
+      center: [37.1750016,-3.6056493],
+      zoom: 13,
+     });
+
+     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      subdomains: 'abcd',
+      minZoom: 1,
+      maxZoom: 19,
+      ext: 'png',
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
+    }).addTo(map);
+
+    let miPopup = L.marker([37.1750016,-3.6056493]).addTo(map);
 
   },
 };
